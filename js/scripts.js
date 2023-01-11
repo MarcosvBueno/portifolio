e0235408353a45740c50e00d9c56b965ce8ac4eb
MarcosvBueno <mvbueno07@gmail.com>
2023-01-11 16:58:56 -0300

    adiciona seção de contatos e correção nas cores

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,39 @@
-const span1 = document.querySelector('#span1')
-const text1 = 'Olá, meu nome é'
-const interval = 80
+const span1 = document.querySelector('#span1');
+const text1 = 'Olá, meu nome é';
+const interval = 80;
 
-const h1_name = document.querySelector('#h1_name')
-const text2 = 'Marcos Bueno'
-const interval2 = 100
+const h1_name = document.querySelector('#h1_name');
+const text2 = 'Marcos Bueno';
+const interval2 = 100;
 
-const span2 = document.querySelector('#span2')
-const text3 = 'sou Desenvolvedor front-end'
-const interval3 = 50
+const span2 = document.querySelector('#span2');
+const text3 = 'sou Desenvolvedor front-end';
+const interval3 = 50;
 
 function showTest(tag, text, interval) {
-  const char = text.split('').reverse()
+  const char = text.split('').reverse();
   const typer = setInterval(() => {
     if (!char.length) {
-      return clearInterval(typer)
+      return clearInterval(typer);
     }
 
-    const next = char.pop()
+    const next = char.pop();
 
-    tag.innerHTML += next
-  }, interval)
+    tag.innerHTML += next;
+  }, interval);
 }
 
-showTest(span1, text1, interval)
-showTest(h1_name, text2, interval2)
-showTest(span2, text3, interval3)
+showTest(span1, text1, interval);
+showTest(h1_name, text2, interval2);
+showTest(span2, text3, interval3);
 
+const data = docuemnt.querySelector('#dataAtual');
 
+const now = new Date();
+const year = now.getFullYear();
+
+showTime = () => {
+  data.innerHTML = `${year}`;
+};
+document.write('hoje é dia');
+showTime();
